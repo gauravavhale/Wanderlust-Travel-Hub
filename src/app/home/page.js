4e6060837c02e8122fd9867354c0ef2adcd65eb3
gaurav avhale <avhalegaurav07@gmail.com>
2024-07-30 12:55:14 +0530

    css

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -10,10 +10,10 @@ const Home = () => {
     <Header />
     <Nav />
     <div className="holidays">
-      <h1 className="the-heading">Enjoy the Holidays</h1>
-      <p className="the-text">Book a ticket and just leave. Lets turn your wanderlust into reality!</p>
-      <p className="the-text">Explore the world with us, What do you think ?</p>
-      <p className="the-text">Book with us and book it out of here!</p>
+      <h1 className="the-head">Enjoy the Holidays</h1>
+      <p className="the-tex">Book a ticket and just leave. Lets turn your wanderlust into reality!</p>
+      <p className="the-tex">Explore the world with us, What do you think ?</p>
+      <p className="the-tex">Book with us and book it out of here!</p>
       <div> <a href="/places" className="btn">Book Now</a></div>
     </div>
     </>
